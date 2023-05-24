44faad5e03f0430e6eb266154947a8bc7aee1214
O-01 <sa.an-s@yandex.com>
2023-05-24 13:32:17 -0700

    8

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printf for with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0 means alles gut, printf printf'ed specified string
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0)
+}
