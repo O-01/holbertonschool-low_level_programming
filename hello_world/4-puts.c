8442c7bcfade3de6873e949fbc5b4afb09e8f420
O-01 <sa.an-s@yandex.com>
2023-05-24 13:22:27 -0700

    6

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - puts message "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 means yes, string was putted.
+ */
+int main(void) /* main - puts things - what do you want, betty */
+{
+char momo[] = "Programming is like building a multilingual puzzle";
+
+puts(momo);
+return (0);
+}
