66e2ea1c38538ff635a25b253d0a68e5b9b7c4da
O-01 <sa.an-s@yandex.com>
2023-05-25 14:38:14 -0700

    34

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 {
 int n;
 
-for (n = 0; n <= 8; ++n)
+for (n = 0; n <= 9; ++n)
 {
 putchar('0' + n);
+if (n <= 8)
+{
 putchar(',');
 putchar(' ');
 }
-putchar('9');
+}
 putchar('\n');
 return (0);
 }
