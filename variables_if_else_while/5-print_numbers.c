55478d956fedba7fee94dd835ccdccb204849d62
O-01 <sa.an-s@yandex.com>
2023-05-25 12:59:57 -0700

    27

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 0 - 9 followed by new line
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; ++n)
+{
+printf("%d\n", n);
+}
+return (0);
+}
