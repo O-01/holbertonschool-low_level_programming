22a6304b0b6fa59f633432178d31e97e2a04bdd9
O-01 <sa.an-s@yandex.com>
2023-05-26 20:55:32 -0700

    42

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - use print_alphabet_x10 to (obvious)
+ * 10 times
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int ppp;
+
+	alpha = 'a';
+	ppp = 1;
+
+	while (ppp <= 10)
+	{
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+	ppp++;
+	}
+}
