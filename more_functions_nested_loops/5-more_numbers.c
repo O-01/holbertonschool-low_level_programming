de24a5162fc3119bdedd881c22051d7928c5de3e
O-01 <sa.an-s@yandex.com>
2023-06-01 10:05:01 -0700

    69

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 10 times
+ *
+ * Return: 0
+ *
+ */
+
+void more_numbers(void)
+{
+	int po;
+	int pu;
+
+	for (pu = 0; pu <=10; pu++)
+	{
+		for (po = 0; po <= 14; po++)
+		{
+			if (po > 9)
+			{
+				_putchar((po / 10) + '0');
+				_putchar((po % 10) + '0');
+			}
+			else
+				_putchar(po + '0');
+		}
+		printf("\n");
+	}
+}
