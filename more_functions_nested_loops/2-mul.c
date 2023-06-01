e812aae0c483b18545857fc813bd86ee852b6052
O-01 <sa.an-s@yandex.com>
2023-06-01 08:58:44 -0700

    66

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplication
+ * @a: int1
+ * @b: int2
+ *
+ *
+ * Return: 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int st;
+
+	st = a * b;
+
+	return (st);
+}
---
+++ more_functions_nested_loops/2-mul.c~
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - addition
+ * @i: int1
+ * @t: int2
+ *
+ *
+ * Return: 0
+ *
+ */
+
+int add(int i, int t)
+{
+	int g;
+
+	g = i + t;
+
+	return (g);
+}
