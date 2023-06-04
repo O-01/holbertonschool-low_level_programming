1028647c63d07c2d42ec1db7366b3bf4800d4e65
O-01 <sa.an-s@yandex.com>
2023-06-04 15:17:03 -0700

    100

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,18 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (n < 0 && n > -2147483648)
 	{
+		n = -(n);
 		_putchar('-');
-		n = -n;
 	}
 
 	if (n / 10)
 		print_number(n / 10);
 
-	_putchar((n % 10) + '0');
+	if (n == -2147483648)
+		_putchar((n % 10) + 64);
+
+	else
+		_putchar((n % 10) + '0');
 }
