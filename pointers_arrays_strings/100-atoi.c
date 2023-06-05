ebe0db3e271faef174b100b72bbb31587659e50f
O-01 <sa.an-s@yandex.com>
2023-06-04 21:52:45 -0700

    101

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: object string
+ *
+ * Return: <undecided for the moment>
+ */
+
+int _atoi(char *s)
+{
+	int a, p;
+
+	if (s
+		return (0);
+
+	else
+	{
+		for (a = 0; s[a] != '\0'; a++)
+		{
+			if (a
+
+		}
+		return (*s);
+	}
+}
