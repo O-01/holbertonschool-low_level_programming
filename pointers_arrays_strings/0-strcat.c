71938ca4395559d59f298b4a1b7928c3132e83c0
O-01 <sa.an-s@yandex.com>
2023-06-07 12:19:07 -0700

    107

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,11 +15,9 @@ char *_strcat(char *dest, char *src)
 	for (x = 0; dest[x] != '\0'; x++);
 
 	for (y = 0; src[y] != '\0'; y++);
-
+/*
 	dest[x] += src[y];
-
-	dest[x] = '\0';
-
+*/
 	return (dest);
 
 }
