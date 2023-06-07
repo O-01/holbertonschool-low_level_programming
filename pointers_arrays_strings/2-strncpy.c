23f8e72b1ddde87dc40b51b8b33c3182a10697e8
O-01 <sa.an-s@yandex.com>
2023-06-07 14:53:34 -0700

    113

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
 
-	if (*src != '\0')
-		for (x = 0; x < n; x++)
-			dest[x] = src[x];
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
 
 	return (dest);
 }
