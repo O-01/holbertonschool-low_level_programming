caf02fccb5678c1012e2753a464ee56a5ba9446d
O-01 <sa.an-s@yandex.com>
2023-06-09 09:21:24 -0700

    128

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * _memcpy - copy n bytes of src to dest
+ * @dest: place where n bytes of src to be copied
+ * @src: source, n bytes of which copied to dest
+ * @n: amount of src to be copied to dest
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
