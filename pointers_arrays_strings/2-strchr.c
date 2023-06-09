33490f8c724297851997a98e79de042c25ada087
O-01 <sa.an-s@yandex.com>
2023-06-09 10:23:43 -0700

    130

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,17 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdio.h>
 
 /**
- * _strchr - 
- *
- *
- *
+ * _strchr - locates character c within string s
+ * @s: object string
+ * @c: character we search
+ * Return: s beginning at c, 0 if not found
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '/0')
-	{
+	while (*s++)
 		if (*s == c)
-			return ((char *)s);
+			return (s);
 
-		s++;
-	}
-
-	return (NULL);
+	return (0);
 }
