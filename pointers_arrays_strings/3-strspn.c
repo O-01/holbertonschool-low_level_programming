8b26789ced4d3df449f29ca8ec692c0d5eb3538e
O-01 <sa.an-s@yandex.com>
2023-06-09 12:21:18 -0700

    132

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strspn - finds length of s1 until it no longer matches s2
+ * @s1: object string 1
+ * @s2: string to be compared against
+ * Return: number of characters until which s1 matches any from s2
  */
 
 unsigned int _strspn(char *s1, char *s2)
@@ -14,10 +14,13 @@ unsigned int _strspn(char *s1, char *s2)
 	unsigned int z = 0;
 
 	for (x = 0; s1[x] != '\0'; x++)
+	{
+		if (z != x)
+			break;
 		for (y = 0; s2[y] != '\0'; y++)
-			if (z < x)
-				if (s1[x] == s2[y])
-					z++;
+			if (s1[x] == s2[y])
+				z++;
+	}
 
 	return (z);
 }
