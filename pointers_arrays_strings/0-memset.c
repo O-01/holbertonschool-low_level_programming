b67f24d9a213501cde31302576655c3f92f66f2b
O-01 <sa.an-s@yandex.com>
2023-06-09 09:06:46 -0700

    126

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * _memset - fills memory s until n positions with b
+ * @s: memory chunk object
+ * @b: what we fill s with until n positions
+ * @n: number of positions of s to fill with b
+ * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
