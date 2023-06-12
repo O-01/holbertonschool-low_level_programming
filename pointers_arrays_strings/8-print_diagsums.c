82df907cdd83d68e6890152d91f7e533ab856f1b
O-01 <sa.an-s@yandex.com>
2023-06-12 16:37:25 -0700

    138

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_diagsums(int *a, int s)
+{
+	int x, y;
+
+	for (x = 0; x < s; x++)
+	{
+		for (y = 0; y < s; y++)
+		{
+			printf("%d, %d\n", ());
+			printf("%d, %d\n", ());
+		}
+		_putchar('\n');
+	}
+}
