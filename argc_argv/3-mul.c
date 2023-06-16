c111c4dfde1bb2c445bdd02d872c2053f096827d
O-01 <sa.an-s@yandex.com>
2023-06-16 11:58:46 -0700

    172

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,11 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int x = argv[1];
-	int y = argv[2];
-
 	if ((argc - 1) == 2)
-		printf("%d\n", x * y);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	else
 		printf("Error");
