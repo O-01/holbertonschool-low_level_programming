8ff48227c8191758a16451275a81c14d209b92f1
O-01 <sa.an-s@yandex.com>
2023-06-16 11:06:37 -0700

    162

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 0;
+
+	if (argc)
+		while (argv[x])
+			x++;
+
+	if (x < 2 || x > 2)
+		printf("Error");
+	else
+		printf("%d\n", (argv[1] * argv[2]));
+
+	return (0);
+}
