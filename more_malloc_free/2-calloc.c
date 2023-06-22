4fd99887e01b2e7f5c9b34cbdc058651b92ad31e
O-01 <sa.an-s@yandex.com>
2023-06-22 13:53:06 -0700

    228

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for array of nm elements of sz bytes each
+ * @nm: array elements for which memory to be allocated
+ * @sz: size of each nm
+ * Return: pointer to allocated memory, NULL if either input is 0 or func NULL
+ */
+
+void *_calloc(unsigned int nm, unsigned int sz)
+{
+	char *uu;
+	unsigned int x = 0;
+
+	if (nm == 0 || sz == 0)
+		return (NULL);
+
+	uu = malloc(nm * sz);
+
+	if (uu == NULL)
+		return (NULL);
+
+	return (uu);
+}
