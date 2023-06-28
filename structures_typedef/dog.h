ea803019fa3dba6bc5b5ad6d8261875f9606949e
O-01 <sa.an-s@yandex.com>
2023-06-28 11:24:46 -0700

    252

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dog - dog attributes
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: pretty much a struct to hold the attributes of a dog
+ */
+
 #ifndef dog_h
 #define dog_h
 
