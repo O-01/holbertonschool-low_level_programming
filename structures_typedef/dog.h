d46488b235b72b7fae70839833a8962d36f75e40
O-01 <sa.an-s@yandex.com>
2023-06-28 13:25:55 -0700

    258

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,14 +13,12 @@
  * Description: pretty much a struct to hold the attributes of a dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-#define dog_t struct dog
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
