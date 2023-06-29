221f3281f013ab80eb01ff72930e5a8bd428bf75
O-01 <sa.an-s@yandex.com>
2023-06-29 16:41:24 -0700

    288

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,8 +22,8 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (*(ops[i].op) != *s)
-		if (ops[i].op != NULL)
+	if (s != NULL)
+		while (ops[i].op != NULL && *(ops[i].op) != *s)
 			i++;
 
 	return (ops[i].f);
