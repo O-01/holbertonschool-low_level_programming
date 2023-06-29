df7a13a1679f80f622de8f624e18cf5fe5d75461
O-01 <sa.an-s@yandex.com>
2023-06-29 11:31:14 -0700

    278

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,6 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int x = 0;
 
 	if (action != NULL)
-		for (; x < size; x++)
-			action(array[x]);
+		if (array != NULL)
+			for (; x < size; x++)
+				action(array[x]);
 }
