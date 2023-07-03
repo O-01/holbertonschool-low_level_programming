4e50e0b7f58e84bb98b6150bd533c0f22a82f8cd
O-01 <sa.an-s@yandex.com>
2023-07-03 12:08:44 -0700

    298

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_all(const char * const frmt, ...)
+{
+	va_list surf;
+	char *dude;
+	int x, y = 0;
+
+	va_start(surf, frmt);
+	while (frmt && frmt[x])
+	{
+		switch (frmt[x])
+		{
+		case 'c':
+			printf("%c", (char) va_arg(surf, int));
+			y++;
+			break;
+		case 'i':
+			printf("%d", va_arg(surf, int));
+			y++;
+			break;
+		case 'f':
+			printf("%f", va_arg(surf, double));
+			y++;
+			break;
+		case 's':
+			dude = va_arg(surf, char *);
+			if (!dude)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", dude);
+			y++;
+			break;
+		}
+		if (y > 0 && frmt[x + 1])
+			printf(", ");
+		x++;
+	}
+	printf("\n");
+	va_end(surf);
+}
