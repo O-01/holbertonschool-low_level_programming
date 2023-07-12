05339b6b093850e41f7decb068da2849f3fb1c67
O-01 <sa.an-s@yandex.com>
2023-07-12 10:52:49 -0700

    305

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints length of strings in nodes
+ * @h: head node
+ * Return: number of elements, -1 if h is NULL
+ */
+
+size_t print_list(const list_t *h)
+{
+	int x = 0;
+	char *s1 = NULL;
+	if (h == NULL)
+		return (-1);
+	for (; h; x++)
+	{
+		s1 = h->str;
+		if (s1)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+	}
+	return (x);
+}
