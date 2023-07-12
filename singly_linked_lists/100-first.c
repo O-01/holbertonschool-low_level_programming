13bae47df19d6ade709ffbabc1ebe940914afddb
O-01 <sa.an-s@yandex.com>
2023-07-12 15:28:11 -0700

    312 tortoise & hare

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void __attribute__ ((constructor)) hare_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
