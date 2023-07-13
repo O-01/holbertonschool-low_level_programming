205975d753c624e43838f30a35094a0101828440
O-01 <sa.an-s@yandex.com>
2023-07-13 15:59:00 -0700

    324 Flip-bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns number of bits that must be toggled until orig = dest
+ * @orig: input number from which bits to be modified
+ * @dest: desired result once bits have been toggled
+ * Return: number of bits to toggle
+ */
+
+unsigned int flip_bits(unsigned long int orig, unsigned long int dest)
+{
+	unsigned long int versus = orig ^ dest;
+	unsigned int sum = 0;
+
+	for (; versus > 0;)
+		sum += (versus & 1), versus >>= 1;
+
+	return (sum);
+}
