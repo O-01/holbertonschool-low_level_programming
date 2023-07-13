bda7e11eda9f25b18b43805174c800e051def95c
O-01 <sa.an-s@yandex.com>
2023-07-13 15:10:41 -0700

    321 Clear-bit function

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets bit at index of input to 0
+ * @input: object number
+ * @index: position within binary of input
+ * Return: 1 upon success, -1 upon error
+ */
+
+int clear_bit(unsigned long int *input, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	if (*input)
+		*input ^= (1 << index);
+
+	return (1);
+}
