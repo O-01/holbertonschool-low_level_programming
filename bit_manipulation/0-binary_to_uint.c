ce559dcf852e870aae7f4e5720c9c910ec77fb75
O-01 <sa.an-s@yandex.com>
2023-07-13 13:23:08 -0700

    317 Betty - decimal-to-binary file

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_pow.c"
 
 /**
  * binary_to_uint - converts binary input string to decimal
@@ -30,22 +31,4 @@ unsigned int binary_to_uint(const char *input)
 	}
 
 	return (taker);
-}
-
-/**
- * _pow - calculates exponent of x to y recursively
- * @x: number of which we calculate to the power of y
- * @y: number to which x is raised exponentially
- * Return: -1 if y is neg, 1 if y is 0, sum if y greater than 0, else 0
- */
-
-int _pow(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	if (y > 0)
-		return (x * _pow(x, (y - 1)));
-	return (0);
 }
--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_binary - prints binary equivalent of base10 input
+ * @input: object number of base10
+ * Return: void
  */
 
-void print_binary(unsigned long int n)
+void print_binary(unsigned long int input)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	if (input > 1)
+		print_binary(input >> 1);
 
-	_putchar((n & 1) + 48);
+	_putchar((input & 1) + 48);
 }
