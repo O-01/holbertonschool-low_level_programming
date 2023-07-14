741a194c038badc00394234f6498b8df19a40814
O-01 <sa.an-s@yandex.com>
2023-07-14 01:38:00 -0700

    328 comb5

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of 2 digit numbers between 00 & 99
+ * Return: 0 upon success
+ */
+
+int main(void)
+{
+	int x = 0, y = 0;
+
+	for (; x < 99; x++)
+	{
+		for (y = x+1; y < 100; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+
+			putchar(' ');
+
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 98 && y == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
