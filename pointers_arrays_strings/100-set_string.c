543478f0f61860c4c116ecfd246a2521a2939c71
O-01 <sa.an-s@yandex.com>
2023-07-14 00:45:45 -0700

    327 Set-string mod

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * set_string - changes string value of first arg to that of second
+ * @s: string to be overwritten
+ * @to: string to write to s
+ * Return: void
  */
 
 void set_string(char **s, char *to)
