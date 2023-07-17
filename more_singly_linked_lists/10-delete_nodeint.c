440ea12f1702aaf0b8d90ee0414f1a77a5867bd9
O-01 <sa.an-s@yandex.com>
2023-07-17 13:23:26 -0700

    363 Successful mod - delete_nodeint index handling

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			if (idx < index && tmp->next == NULL)
 				return (-1);
-			if (idx == index)
+			if (idx == index - 1)
 			{
 				del = tmp->next;
 				if (tmp->next != NULL)
