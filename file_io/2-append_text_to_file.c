895c80ebc9de0a58ce438e801333500ebf709e72
O-01 <sa.an-s@yandex.com>
2023-07-17 00:44:34 -0700

    338 Append-text added

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends input text to specified file
+ * @file: object file
+ * @text: text to be input to file
+ * Return: 1 upon success, -1 upon file unspecified or open/write failure
+ */
+
+int append_text_to_file(const char *file, char *text)
+{
+	int fd = 0, strlen_sub = 0;
+
+	if (file == NULL)
+		return (-1);
+
+	fd = open(file, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text)
+	{
+		for (; text[strlen_sub]; strlen_sub++)
+			;
+
+		if (write(fd, text, strlen_sub) == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
