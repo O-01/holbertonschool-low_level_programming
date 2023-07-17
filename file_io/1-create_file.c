41dd8c62608c300bb8073f4f396a671bb3dbd72b
O-01 <sa.an-s@yandex.com>
2023-07-17 00:30:58 -0700

    337 Mod create-file to accomodate NULL text input + Betty formatting

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * create_file - creates file with input text
+ * @file: filename of created file
+ * @text: intended content of file
+ * Return: 1 upon success, -1 upon NULL filename or create/write failure
  */
 
 int create_file(const char *file, char *text)
@@ -20,15 +20,18 @@ int create_file(const char *file, char *text)
 	if (fd == -1)
 		return (-1);
 
-	for (; text[strlen_sub]; strlen_sub++)
+	if (text)
+	{
+		for (; text[strlen_sub]; strlen_sub++)
 			;
 
-	out = write(fd, text, strlen_sub);
+		out = write(fd, text, strlen_sub);
+
+		if (out == -1 || out != strlen_sub)
+			return (-1);
+	}
 
 	close(fd);
-
-	if (out == -1 || out != strlen_sub)
-		return (-1);
 
 	return (1);
 }
