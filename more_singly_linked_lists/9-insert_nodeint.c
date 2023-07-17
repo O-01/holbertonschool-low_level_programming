b562cef2d1d81578146425fe3ab849c1cc6a4053
O-01 <sa.an-s@yandex.com>
2023-07-17 14:06:44 -0700

    365 Modified insert_nodeint - correct output

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,23 +15,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	add = malloc(sizeof(listint_t));
 
-	if ((!(*head) && idx) || add == NULL)
+	if (add == NULL)
 		return (NULL);
 
 	tmp = *head;
 
-	for (; tmp->next; id_x++)
+	for (; tmp->next && id_x != idx; id_x++)
 	{
 		if (id_x < idx && tmp->next == NULL)
 			return (NULL);
-		if (id_x == idx)
-		{
-			add->n = n;
-			add->next = tmp->next;
-			tmp->next = add;
-			continue;
-		}
 		tmp = tmp->next;
 	}
+
+	add->n = n;
+	add->next = tmp->next;
+	tmp->next = add;
+
 	return (add);
 }
