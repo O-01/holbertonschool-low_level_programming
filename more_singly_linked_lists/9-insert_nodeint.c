b2bf69538dff60eab6047d2c4e069766e929b8b6
O-01 <sa.an-s@yandex.com>
2023-07-17 13:54:39 -0700

    364 Test insert_nodeint as-is

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,3 +10,28 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *tmp, *add;
+	unsigned int id_x;
+
+	add = malloc(sizeof(listint_t));
+
+	if ((!(*head) && idx) || add == NULL)
+		return (NULL);
+
+	tmp = *head;
+
+	for (; tmp->next; id_x++)
+	{
+		if (id_x < idx && tmp->next == NULL)
+			return (NULL);
+		if (id_x == idx)
+		{
+			add->n = n;
+			add->next = tmp->next;
+			tmp->next = add;
+			continue;
+		}
+		tmp = tmp->next;
+	}
+	return (add);
+}
