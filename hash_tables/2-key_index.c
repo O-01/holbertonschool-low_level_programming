79e2b3a26d90e0b542caafdf244fa4ada227c94c
O-01 <sa.an-s@yandex.com>
2023-07-21 11:54:45 -0700

    399 Added key_index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - finds the index of given key
+ * @key: key of which we wish to find the index
+ * @size: size of the hash table array
+ * Return: index of given key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idx, hash;
+
+	hash = hash_djb2(key);
+	idx = hash % size;
+
+	return (idx);
+}
