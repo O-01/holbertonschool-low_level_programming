d3957496ba9ab9d6672712b16b20ea379d431289
O-01 <sa.an-s@yandex.com>
2023-07-21 13:05:15 -0700

    402 Betty formatting - hash_table_set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (add == NULL || key == NULL || ht == NULL)
 		return (0);
 
-	for(; key[x]; x++)
+	for (; key[x]; x++)
 		;
 	if (x < 1)
 		return (0);
