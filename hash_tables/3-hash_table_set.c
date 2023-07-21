dbf6ae51b50907cb13958df4f06154edad07eca7
O-01 <sa.an-s@yandex.com>
2023-07-21 13:30:49 -0700

    406 Testing hash_table_set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -44,12 +44,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(add->key);
 				free(add->value);
 				free(add);
-				goto ret;
+				return (1);
 			}
 		}
 		add->next = head;
 		ht->array[idx] = add;
 	}
-ret:
 	return (1);
 }
