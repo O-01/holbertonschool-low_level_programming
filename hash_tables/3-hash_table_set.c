d954a4a30334753574b4988953cce0e8bf688e77
O-01 <sa.an-s@yandex.com>
2023-07-21 16:09:21 -0700

    412 Optimized hash_table_print

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,43 +10,35 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *add = NULL, *head = NULL, *tmp = NULL;
-	unsigned long int idx = 0, x = 0;
+	hash_node_t *add = NULL, *tmp = NULL;
+	unsigned long int idx = 0;
 
-	add = malloc(sizeof(hash_node_t));
-	if (add == NULL || key == NULL || ht == NULL)
+	if (ht == NULL || strlen(key) == 0)
 		return (0);
+
 	if (value == NULL)
 		value = "";
 
-	for (; key[x]; x++)
-		;
-	if (x < 1)
+	idx = (hash_djb2((const unsigned char *)key)) % ht->size;
+
+	for (tmp = ht->array[idx]; tmp; tmp = tmp->next)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+	}
+
+	add = malloc(sizeof(hash_node_t));
+	if (add == NULL)
 		return (0);
 
 	add->key = strdup(key);
 	add->value = strdup(value);
-	add->next = NULL;
+	add->next = ht->array[idx];
+	ht->array[idx] = add;
 
-	idx = (hash_djb2((const unsigned char *)key)) % ht->size;
-	if (ht->array[idx] == NULL)
-		ht->array[idx] = add;
-	else
-	{
-		head = ht->array[idx];
-		for (tmp = head; tmp; tmp = tmp->next)
-		{
-			if (strcmp(key, tmp->key) == 0)
-			{
-				free(tmp->value);
-				tmp->value = strdup(value);
-				free(add->key);
-				free(add->value);
-				free(add);
-				return (1);
-			}
-		}
-		add->next = head, ht->array[idx] = add;
-	}
 	return (1);
 }
