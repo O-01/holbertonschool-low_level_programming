5e574a5634cac3b63dd7ec11498dc56b95375e60
O-01 <sa.an-s@yandex.com>
2023-07-21 12:56:54 -0700

    401 Modified hash_table_set to accommodate NULL ht

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
- *
- *
+ * hash_table_set - adds an element to hash table
+ * @ht: object hash table
+ * @key: key of node to be added
+ * @value: value of node to be added
+ * Return: 1 upon success, 0 upon failure
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	add = malloc(sizeof(hash_node_t));
 
-	if (add == NULL || key == NULL)
+	if (add == NULL || key == NULL || ht == NULL)
 		return (0);
 
 	for(; key[x]; x++)
