ba94fc0c65350eb6d5a247bc50efb6ba7488e8d7
O-01 <sa.an-s@yandex.com>
2023-07-21 18:37:10 -0700

    415 Improved hash_table_delete

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,21 +8,24 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp = NULL, *fre = NULL;
+	hash_node_t *node = NULL, *delete = NULL;
 	unsigned long int x = 0;
 
-	if (ht == NULL)
+	if (!ht || !ht->size || !ht->array)
 		return;
 
-	for (; x < ht->size; x++)
-		for (tmp = ht->array[x]; tmp != NULL; tmp = tmp->next)
+	for (; x != ht->size; x++)
+		for (node = ht->array[x]; node; node = node->next)
 		{
-			fre = tmp;
-			free(fre->key);
-			free(fre->value);
-			free(fre);
+			delete = node;
+			free(delete->key);
+			free(delete->value);
+			free(delete);
 		}
 
 	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+
 	free(ht);
 }
