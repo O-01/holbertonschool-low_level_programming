674cb352eb6727d021f9dffe174f65cca375fa5c
O-01 <sa.an-s@yandex.com>
2024-09-30 04:51:20 -0500

    Updated README, minor cleanup and modifications (search_algorithms)

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,20 +7,17 @@
  * @value: value to be sought within array
  * Return: index where/if value found, -1 upon value not found or NULL array
  */
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
 	if (!array || !size)
 		return (-1);
-
 	for (; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
-
 	return (-1);
 }
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -7,20 +7,16 @@
  * @value: value to be sought within array
  * Return: index where/if value found, -1 upon value not found or NULL array
  */
-
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = 0, mid = 0;
 
 	if (!array || !size)
 		return (-1);
-
-	for (left = 0, right = size - 1;
-	     left <= right;)
+	for (left = 0, right = size - 1; left <= right;)
 	{
 		printf("Searching in array: ");
 		print_array(&array[left], (right - left + 1));
-
 		mid = (left + right) / 2;
 		if (array[mid] < value)
 			left = mid + 1;
@@ -28,7 +24,6 @@ int binary_search(int *array, size_t size, int value)
 			right = mid - 1;
 		else
 			return (mid);
-
 	}
 	return (-1);
 }
@@ -38,16 +33,11 @@ int binary_search(int *array, size_t size, int value)
  * @array: object array
  * @size: size of array
  */
-
 void print_array(const int *array, size_t size)
 {
 	size_t i = 0;
 
-	for (i = 0; array && i < size; ++i)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-	}
+	for (; array && i < size; ++i)
+		printf("%d%s", array[i], i + 1 < size ? ", " : "");
 	printf("\n");
 }
